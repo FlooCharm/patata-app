c84f908c3dd9503db6e24f1c2262d30539f23cbd
Jessica Sánchez <alumno@sldid10-3.local>
2019-03-04 07:51:56 -0600

    added classes

--- web/patata-app/src/App.js
+++ web/patata-app/src/App.js
@@ -1,5 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import Corgi, { eats } from './corgi';
+
+class Person {
+	constructor(name) {
+		this.name = name;
+	}
+
+	talk () {
+		console.log(this);
+		return true;
+	}
+}
+
+let person = new Person('Josh');
+person.talk();
+console.log(person.talk());
+
+class Student extends Person {
+	constructor(name, studentId) {
+		super(name);
+		this.studentId = studentId;
+	}
+}
+
+let lulu = new Student('lulu', '123');
+console.log(lulu)
+
+let corgi = new Corgi('Thor');
+console.log(corgi)
+console.log(eats())
 
 class App extends Component {
 	render() {
@@ -12,6 +42,7 @@ class App extends Component {
 				console.log(this)
 			}
 		}
+		// ()=>{} doesn't create 'this'
 		const dog = {
 			eat() {
 				setTimeout(() => {
@@ -19,13 +50,42 @@ class App extends Component {
 				}, 1000);
 			}
 		}
+		// destructuring and spread syntax
+		let colors = ['blue', 'red', 'green'];
+		let missing_colors = ['orange'];
+		let all_colors = [...colors, ...missing_colors].join(', ');
+
+		// const red = jingleBells.red;
+		// const blue = jingleBells.blue;
+		// const orange = jingleBells.orange;
+		let jingleBells = {
+			red: 2,
+			blue: 3,
+			orange: 5
+		}
+		let newJingleBells = { ...jingleBells };
+		console.log(newJingleBells)
+		const { red, blue, orange } = jingleBells;
+
 		return (
 			<div>
 				<div className='header'>
 					wa
 				</div>
 				<div className='body'>
-					{array}
+					<div>
+						array: {array}
+					</div>
+					<div>
+						colors: {all_colors}
+					</div>
+					<div>
+						red: {red}
+						<br />
+						blue: {blue}
+						<br />
+						orange: {orange}
+					</div>
 					<button onClick={() => person.tick()}>Show person</button>
 					<button onClick={() => dog.eat()}>Dog eats</button>
 				</div>
---
+++ web/patata-app/src/corgi.js
@@ -0,0 +1,8 @@
+export default class Corgi {
+	constructor(name) {
+		this.name = name;
+	}
+}
+export function eats() {
+	console.log('is eating');
+}
